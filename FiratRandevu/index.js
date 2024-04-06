4e1480e6cc6dbdafd6df89003a8d615c86b346e0
Arda <94324501+Ardacanuysal@users.noreply.github.com>
2024-04-06 16:35:19 +0300

    AkademsyenLogin screen Added

--- FiratRandevu/index.js
+++ FiratRandevu/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/screens/login/LoginOgrenci';
+import App from './src/screens/login/LoginAkademik';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
