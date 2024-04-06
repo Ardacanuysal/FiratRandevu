55021d7084dfbf58661abd1d2995b63d8c507126
Arda <94324501+Ardacanuysal@users.noreply.github.com>
2024-04-06 22:06:45 +0300

    Academic Kadro Page Complated

--- FiratRandevu/src/screens/Academic/AcademicKadro.jsx
+++ FiratRandevu/src/screens/Academic/AcademicKadro.jsx
@@ -6,6 +6,7 @@ import {
   Dimensions,
   StyleSheet,
   Image,
+  ScrollView,
 } from 'react-native';
 
 const windowWidth = Dimensions.get('window').width;
@@ -15,7 +16,7 @@ const rectangleWidth = windowWidth * 0.9;
 export class Main extends Component {
   render() {
     return (
-      <SafeAreaView>
+      <ScrollView>
         <View style={styles.headerContainer}>
           <View style={styles.FiratUniversity}>
             <View style={styles.FiratUniversity}>
@@ -30,7 +31,7 @@ export class Main extends Component {
         <View style={styles.rectangleContainer}>
           <View style={[styles.rectangle, {width: rectangleWidth}]}>
             <View style={styles.infoContainer}>
-              <Text style={styles.studentName}>Arda Can Usyal</Text>
+              <Text style={styles.studentName}>Arda Can Uysal</Text>
               <Text style={styles.department}>Yazılım Mühendisliği</Text>
               <Text style={styles.grade}>3. Sınıf</Text>
             </View>
@@ -40,11 +41,139 @@ export class Main extends Component {
             <Text style={styles.role}>Akademik Kadro</Text>
           </View>
         </View>
-      </SafeAreaView>
+        <View
+          style={{backgroundColor: 'gray', height: windowWidth * 0.002}}></View>
+        <View>
+          <View style={styles.Card}>
+            <Image
+              style={styles.infoTeachimg}
+              source={require('../../assets/images/engin.png')}></Image>
+            <View style={styles.infoTeachbox}>
+              <Text style={styles.infoTeachtxt}>Bölüm Başkanı Prof. Dr.</Text>
+              <Text style={styles.infoTeachName}>Engin Avci</Text>
+              <Text style={styles.infoTeachMail}>
+                email: enginavci@firat.edu.tr
+              </Text>
+              <View style={{flexDirection: 'row'}}>
+                <Image
+                  style={styles.infoTeachIcons}
+                  source={require('../../assets/icons/linkedin.png')}></Image>
+                <Image
+                  style={styles.infoTeachIcons}
+                  source={require('../../assets/icons/medium.png')}></Image>
+              </View>
+            </View>
+            <View style={styles.cardBtn}>
+              <View style={styles.Btn}>
+                <Text style={styles.btnTxt}>Randevu Al</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.Card}>
+            <Image
+              style={styles.infoTeachimg}
+              source={require('../../assets/images/engin.png')}></Image>
+            <View style={styles.infoTeachbox}>
+              <Text style={styles.infoTeachtxt}>Bölüm Başkanı Prof. Dr.</Text>
+              <Text style={styles.infoTeachName}>Engin Avci</Text>
+              <Text style={styles.infoTeachMail}>
+                email: enginavci@firat.edu.tr
+              </Text>
+              <View style={{flexDirection: 'row'}}>
+                <Image
+                  style={styles.infoTeachIcons}
+                  source={require('../../assets/icons/linkedin.png')}></Image>
+                <Image
+                  style={styles.infoTeachIcons}
+                  source={require('../../assets/icons/medium.png')}></Image>
+              </View>
+            </View>
+            <View style={styles.cardBtn}>
+              <View style={styles.Btn}>
+                <Text style={styles.btnTxt}>Randevu Al</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.Card}>
+            <Image
+              style={styles.infoTeachimg}
+              source={require('../../assets/images/engin.png')}></Image>
+            <View style={styles.infoTeachbox}>
+              <Text style={styles.infoTeachtxt}>Bölüm Başkanı Prof. Dr.</Text>
+              <Text style={styles.infoTeachName}>Engin Avci</Text>
+              <Text style={styles.infoTeachMail}>
+                email: enginavci@firat.edu.tr
+              </Text>
+              <View style={{flexDirection: 'row'}}>
+                <Image
+                  style={styles.infoTeachIcons}
+                  source={require('../../assets/icons/linkedin.png')}></Image>
+                <Image
+                  style={styles.infoTeachIcons}
+                  source={require('../../assets/icons/medium.png')}></Image>
+              </View>
+            </View>
+            <View style={styles.cardBtn}>
+              <View style={styles.Btn}>
+                <Text style={styles.btnTxt}>Randevu Al</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.Card}>
+            <Image
+              style={styles.infoTeachimg}
+              source={require('../../assets/images/engin.png')}></Image>
+            <View style={styles.infoTeachbox}>
+              <Text style={styles.infoTeachtxt}>Bölüm Başkanı Prof. Dr.</Text>
+              <Text style={styles.infoTeachName}>Engin Avci</Text>
+              <Text style={styles.infoTeachMail}>
+                email: enginavci@firat.edu.tr
+              </Text>
+              <View style={{flexDirection: 'row'}}>
+                <Image
+                  style={styles.infoTeachIcons}
+                  source={require('../../assets/icons/linkedin.png')}></Image>
+                <Image
+                  style={styles.infoTeachIcons}
+                  source={require('../../assets/icons/medium.png')}></Image>
+              </View>
+            </View>
+            <View style={styles.cardBtn}>
+              <View style={styles.Btn}>
+                <Text style={styles.btnTxt}>Randevu Al</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.Card}>
+            <Image
+              style={styles.infoTeachimg}
+              source={require('../../assets/images/engin.png')}></Image>
+            <View style={styles.infoTeachbox}>
+              <Text style={styles.infoTeachtxt}>Bölüm Başkanı Prof. Dr.</Text>
+              <Text style={styles.infoTeachName}>Engin Avci</Text>
+              <Text style={styles.infoTeachMail}>
+                email: enginavci@firat.edu.tr
+              </Text>
+              <View style={{flexDirection: 'row'}}>
+                <Image
+                  style={styles.infoTeachIcons}
+                  source={require('../../assets/icons/linkedin.png')}></Image>
+                <Image
+                  style={styles.infoTeachIcons}
+                  source={require('../../assets/icons/medium.png')}></Image>
+              </View>
+            </View>
+            <View style={styles.cardBtn}>
+              <View style={styles.Btn}>
+                <Text style={styles.btnTxt}>Randevu Al</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
-} 
-
+}
 const styles = StyleSheet.create({
   headerContainer: {
     backgroundColor: '#78113E',
@@ -109,6 +238,59 @@ const styles = StyleSheet.create({
   },
   role: {
     color: 'black',
+  },
+  Card: {
+    backgroundColor: '#EEEEEE',
+    flexDirection: 'row',
+    marginTop: windowWidth * 0.008,
+    width: windowWidth * 1,
+    height: windowWidth * 0.3,
+  },
+  infoTeachimg: {
+    marginLeft: windowWidth * 0.01,
+    width: windowWidth * 0.3,
+    height: windowWidth * 0.3,
+  },
+  infoTeachbox: {
+    marginTop: windowWidth * 0.02,
+    paddingLeft: windowWidth * 0.02,
+  },
+  infoTeachtxt: {
+    fontSize: windowWidth * 0.02,
+    color: '#78113E',
+  },
+  infoTeachName: {
+    fontSize: windowWidth * 0.06,
+    color: '#78113E',
+  },
+  infoTeachMail: {
+    fontWeight: '600',
+    fontsize: windowWidth * 0.03,
+  },
+  infoTeachIcons: {
+    width: windowWidth * 0.05,
+    height: windowWidth * 0.05,
+    marginLeft: windowWidth * 0.01,
+  },
+  cardBtn: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: windowWidth * 0.22,
+    marginHorizontal: windowWidth * -0.2,
+  },
+  Btn: {
+    backgroundColor: '#78113E',
+    width: windowWidth * 0.3,
+    height: windowWidth * 0.06,
+    borderRadius: windowWidth * 1,
+  },
+  btnTxt: {
+    paddingTop: windowWidth * 0.01,
+    alignSelf: 'center',
+    margin: 'center',
+    alignItems: 'center',
+    color: '#fff',
+    fontsize: windowWidth * 0.09,
   },
 });
 
