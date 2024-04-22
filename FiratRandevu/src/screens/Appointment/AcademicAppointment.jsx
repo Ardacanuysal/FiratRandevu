5dceba9cc62895796db1bc2c44eaef3dec653ebc
Arda <94324501+Ardacanuysal@users.noreply.github.com>
2024-04-22 10:28:27 +0300

    Acedemician Card
    Academician Card added Calendar Pack updated

--- FiratRandevu/src/screens/Appointment/AcademicAppointment.jsx
+++ FiratRandevu/src/screens/Appointment/AcademicAppointment.jsx
@@ -1,4 +1,11 @@
-import {StyleSheet, Text, View, Dimensions, Image} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 import React from 'react';
 
 const windowWidth = Dimensions.get('window').width;
@@ -24,213 +31,14 @@ const AcademicAppointment = () => {
           </View>
         </View>
       </View>
-      {/* I'll leave the grid generation to you, since it's quite lengthy */}
-      <View style={{backgroundColor: '#fff', height: windowHeight}}>
-        <View style={{flexDirection: 'row'}}>
-          <View style={{margin: windowWidth * 0.1,shadowOpacity:0.5}}>
-            <Image
-              style={styles.nextPervius}
-              source={require('../../assets/icons/pervius.png')}
-            />
-          </View>
-          <View style={{margin: windowWidth * 0.06}}>
-            <Text style={{fontSize: windowWidth * 0.08, fontWeight: '700'}}>
-              October
-            </Text>
-          </View>
-          <View style={{margin: windowWidth * 0.1,shadowOpacity:0.5}}>
-            <Image
-              style={styles.nextPervius}
-              source={require('../../assets/icons/next.png')}
-            />
-          </View>
-          <View style={{margin: windowWidth * 0.06}}></View>
+      <ScrollView>
+        <View style={styles.Card}>
+          <Image
+            style={styles.img}
+            source={require('../../assets/images/ibrahimtrkogl.png')}></Image>
+          <View style={styles.gryCard}></View>
         </View>
-        {/* I'll leave the grid generation to you, since it's quite lengthy */}
-        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-          <View>
-            <Text style={{fontWeight: '700'}}>Pt</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>Sa</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>Çar</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>Per</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>Cum</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>Cts</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>Pz</Text>
-          </View>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            marginTop: windowWidth * 0.08,
-            marginLeft: windowWidth * 0.04,
-          }}>
-          <View>
-            <Text style={{fontWeight: '700'}}></Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}></Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}></Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>1</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>2</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>3</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>4</Text>
-          </View>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            marginTop: windowWidth * 0.08,
-            marginLeft: windowWidth * 0.03,
-          }}>
-          <View>
-            <Text style={{fontWeight: '700'}}>5</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>6</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}> 7</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}> 8</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}> 9</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}> 10</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}> 11</Text>
-          </View>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            marginTop: windowWidth * 0.08,
-            marginLeft: windowWidth * 0.01,
-          }}>
-          <View>
-            <Text style={{fontWeight: '700'}}>12</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>13</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>14</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>15</Text>
-          </View>
-          <View
-            style={{
-              backgroundColor: '#78113E',
-              width: windowWidth * 0.1,
-              height: windowWidth * 0.1,
-              borderRadius: windowWidth * 0.03,
-              position: 'relative',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginTop: -windowWidth * 0.02,
-            }}>
-            <Text
-              style={{
-                fontWeight: '700',
-                color: 'white',
-              }}>
-              16
-            </Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>17</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>18</Text>
-          </View>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            marginTop: windowWidth * 0.04,
-            marginLeft: windowWidth * 0.01,
-          }}>
-          <View>
-            <Text style={{fontWeight: '700'}}>19</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>20</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>21</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>22</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>23</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>24</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>25</Text>
-          </View>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            marginTop: windowWidth * 0.08,
-            marginLeft: windowWidth * 0.01,
-          }}>
-          <View>
-            <Text style={{fontWeight: '700'}}>26</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>27</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>28</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>29</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>30</Text>
-          </View>
-          <View>
-            <Text style={{fontWeight: '700'}}>31</Text>
-          </View>
-          <View></View>
-          {/* Diğer takvim kısımları buraya gelecek */}
-        </View>
-      </View>
+      </ScrollView>
     </View>
   );
 };
@@ -275,8 +83,33 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     marginTop: windowWidth * 0.1,
   },
-  nextPervius: {
-    width: windowWidth * 0.09,
-    height: windowWidth * 0.09,
+  Card: {
+    backgroundColor: '#78113E',
+    marginTop: windowWidth * 0.03,
+    marginLeft: windowWidth * 0.03,
+    marginRight: windowWidth * 0.3,
+    width: windowWidth * 0.94,
+    height: windowWidth * 0.3,
+    borderRadius: windowWidth * 0.02,
+    flexDirection: 'row',
+  },
+  img: {
+    width: windowWidth * 0.25,
+    height: windowWidth * 0.25,
+    marginLeft: windowWidth * 0.03,
+    alignSelf: 'center',
+    borderBottomStartRadius: windowWidth * 0.03,
+    borderTopLeftRadius: windowWidth * 0.03,
+  },
+  gryCard: {
+    alignSelf: 'center',
+    backgroundColor: '#D9D9D9',
+    width: windowWidth * 0.63,
+    height: windowWidth * 0.25,
+    borderEndEndRadius: windowWidth * 0.03,
+    borderTopRightRadius: windowWidth * 0.03,
+    borderWidth: windowWidth * 0.01,
+    borderLeftWidth: windowWidth * 0,
+    borderColor: '#ffff',
   },
 });
